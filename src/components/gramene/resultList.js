78adf26ad596b7203de70d96e083f9d2e1c7dc9d
Andrew Olson <olson@cshl.edu>
2018-07-06 17:05:44 -0400

    use public search_api Add gramene views skeleton code

--- src/components/gramene/resultList.js
+++ src/components/gramene/resultList.js
@@ -1,21 +1,81 @@
-import { connect } from 'redux-bundler-preact'
-import { h } from 'preact'
+import {connect} from 'redux-bundler-preact'
+import {h} from 'preact'
 
-const Genes = results => (
-  <p id="Genes">show Genes here</p>
-);
-const Pathways = results => (
-  <p id="Pathways">show Pathways here</p>
-);
-const Domains = results => (
-  <p id="Domains">show Domains here</p>
-);
-const Species = results => (
-  <p id="Species">show Species here</p>
-);
+const Gene = ({gene}) => {
+  return <div className="row">{gene.id}</div>
+};
+
+const Genes = results => {
+  if (results && results.numFound > 0) {
+    return (
+      <div id="Genes" className="container pt90">
+        <div className="fancy-title">
+          <h4>Genes</h4>
+        </div>
+        {results.response.docs.map(doc => (
+          <Gene gene={doc}/>
+        ))}
+      </div>
+    );
+  }
+};
+
+const Pathway = ({pathway}) => {
+  return <div className="row">{pathway.name}</div>
+};
+const Pathways = results => {
+  if (results && results.numFound > 0) {
+    return (
+      <div id="Pathways" className="container pt90">
+        <div className="fancy-title">
+          <h4>Pathways</h4>
+        </div>
+        {results.pathways.map(doc => (
+          <Pathway pathway={doc}/>
+        ))}
+      </div>
+    );
+  }
+};
+
+const Domain = ({domain}) => {
+  return <div className="row">{domain.id}</div>
+};
+const Domains = results => {
+  if (results && results.numFound > 0) {
+    return (
+      <div id="Domains" className="container pt90">
+        <div className="fancy-title">
+          <h4>Domains</h4>
+        </div>
+        {results.domains.map(doc => (
+          <Domain domain={doc}/>
+        ))}
+      </div>
+    );
+  }
+};
+
+const Taxon = ({taxon}) => {
+  return <div className="row">{taxon.id}</div>
+};
+const Species = results => {
+  if (results && results.numFound > 0) {
+    return (
+      <div id="Species" className="container pt90">
+        <div className="fancy-title">
+          <h4>Species</h4>
+        </div>
+        {results.taxonomy.map(doc => (
+          <Taxon taxon={doc}/>
+        ))}
+      </div>
+    );
+  }
+};
 
 const ResultsList = ({grameneGenes, grameneDomains, gramenePathways, grameneTaxonomy, searchUI, searchUpdated}) => (
-  <div id="gramene" class="pt50 row">
+  <div id="gramene" class="row">
     <div class="col">
       <div>
         {searchUI.Gramene && searchUI.Genes && Genes(grameneGenes)}
