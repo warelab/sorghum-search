a17cc28ceee1622e4f4bbbd9b9739a77f5aeeff8
Andrew Olson <olson@cshl.edu>
2018-07-11 12:03:29 -0400

    refactoring

--- index.js
+++
@@ -1,4 +0,0 @@
-import bundles from './src/bundles'
-import resultList from './src/resultList'
-import resultSummary from './src/resultSummary'
-export {bundles, resultList, resultSummary}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import bundles from './bundles'
+import resultList from './resultList'
+import resultSummary from './resultSummary'
+export {bundles, resultList, resultSummary}
